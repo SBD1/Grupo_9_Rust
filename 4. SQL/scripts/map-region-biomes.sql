27b775bf54d513e30cc064c308bf968732356ee0
gabrielbpn <180145088@aluno.unb.br>
2022-03-26 15:53:48 -0300

    creating table of map, region and biomes

---
+++ 4. SQL/scripts/map-region-biomes.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Map ( 
+    id SERIAL PRIMARY KEY UNIQUE,
+    mapID int,
+);
+
+CREATE TABLE Region ( 
+    name VARCHAR(30),
+    coordinates int,
+    biome VARCHAR(30),
+    dangerLevel SMALLINT,
+    monument VARCHAR(30),
+    FOREIGN KEY (monument) REFERENCES Monuments(name) ON DELETE RESTRICT
+);
+
+CREATE TABLE Biomes ( 
+  biomesID int,
+  coordinates int,
+  resourceAbundance ENUM_BOOLEAN,
+  resourceAvailability ENUM_BOOLEAN,
+  type VARCHAR(30),
+  climate int,
+);
