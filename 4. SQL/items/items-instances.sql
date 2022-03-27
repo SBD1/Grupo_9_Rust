b3a9239dd93c17dab7bb6b369c08ad2dfcf53c18
Thiago <thiagoiferreira@outlook.com>
2022-03-27 03:51:42 -0300

    adding consumables, fixing inputs where i set numerical values in string format

--- 4. SQL/items/items-instances.sql
+++ 4. SQL/items/items-instances.sql
@@ -20,40 +20,40 @@ INSERT INTO Resources (maxStackSize,isPrimary, itemType,itemName,isRefinable,loo
 INSERT INTO RangedWeapons(maxStackSize, itemName, itemType, lootGrade, 
 attackRange, headDamage,chestDamage,legDamage,armsDamage, accuracyModifier, fireRate,
 fireMode, modSlots, ammoCapacity, recoil) VALUES
-(1,'Hunting Bow','ranged','basic','150','80', '50','35','40','0.5','0.5','single','0','1','0'),
-(1,'Crossbow','ranged','basic','200','102', '60','45','45','0.75','0.3','single','2','1','0'),
-(1,'Compound Bow','ranged','basic','250','100', '50','40','40','0.65','0.5','charged','0','1','0'),
-(1,'Waterpipe Shotgun','ranged','basic','10','210', '180','75','75','1','0.4','single','0','1','2'),
-(1,'Pump Shotgun','ranged','military','10','280', '210','80','80','1','1','pump','2','6','2'),
-(1,'Eoka Pistol','ranged','basic','10','210', '180','75','75','1','0.2','single','0','1','2'),
-(1,'Double Barrel Shotgun','ranged','basic','10','210', '180','75','75','1','1','semi-auto','2','2','2'),
-(1,'Nailgun','ranged','basic','60','36', '18','12','8','2','3','semi-auto','0','16','0.5'),
-(1,'Python Revolver','ranged','military','350','110', '55','45','45','2','2','semi-auto','3','6','4'),
-(1,'Revolver','ranged','basic','250','70', '35','26','26','2','2','semi-auto','1','8','1.5'),
-(1,'Custom SMG','ranged','military','200','60', '35','30','35','2','3','full-auto','3','24','3'),
-(1,'Thompson','ranged','military','250','76', '38','30','30','2.5','2.5','full-auto','3','20','2.8'),
-(1,'MP5A4','ranged','elite','350','70', '35','29','35','3','2.5','full-auto','3','30','2'),
-(1,'Semi-Automatic Pistol','ranged','military','250','80', '40','30','40','3','1.5','semi-auto','3','10','1.5'),
-(1,'Semi-Automatic Rifle','ranged','military','350','80', '40','30','40','3','1.5','semi-auto','3','16','2'),
-(1,'LR-300 Assault Rifle','ranged','elite','400','80', '40','30','40','3','2.5','semi-auto','3','30','2'),
-(1,'AK47 Assault Rifle','ranged','elite','450','85', '45','35','45','3','3','full-auto','3','30','3.5'),
-(1,'Bolt Action Rifle','ranged','elite','900','150', '75','50','50','5','0.8','single','3','4','5');
+(1,'Hunting Bow','ranged','basic',150,80, 50,35,40,0.5,0.5,'single',0,1,0),
+(1,'Crossbow','ranged','basic',200,102, 60,45,45,0.75,0.3,'single',2,1,0),
+(1,'Compound Bow','ranged','basic',250,100, 50,40,40,0.65,0.5,'charged',0,1,0),
+(1,'Waterpipe Shotgun','ranged','basic',10,210,180,75,75,1,0.4,'single',0,1,2),
+(1,'Pump Shotgun','ranged','military',10,280, 210,80,80,1,1,'pump',2,6,2),
+(1,'Eoka Pistol','ranged','basic',10,210, 180,75,75,1,0.2,'single',0,1,2),
+(1,'Double Barrel Shotgun','ranged','basic',10,210, 180,75,75,1,1,'semi-auto',2,2,2),
+(1,'Nailgun','ranged','basic',60,36, 18,12,8,2,3,'semi-auto',0,16,0.5),
+(1,'Python Revolver','ranged','military',350,110, 55,45,45,2,2,'semi-auto',3,6,4),
+(1,'Revolver','ranged','basic',250,70, 35,26,26,2,2,'semi-auto',1,8,1.5),
+(1,'Custom SMG','ranged','military',200,60, 35,30,35,2,3,'full-auto',3,24,3),
+(1,'Thompson','ranged','military',250,76, 38,30,30,2.5,2.5,'full-auto',3,20,2.8),
+(1,'MP5A4','ranged','elite',350,70, 35,29,35,3,2.5,'full-auto',3,30,2),
+(1,'Semi-Automatic Pistol','ranged','military',250,80, 40,30,40,3,1.5,'semi-auto',3,10,1.5),
+(1,'Semi-Automatic Rifle','ranged','military',350,80, 40,30,40,3,1.5,'semi-auto',3,16,2),
+(1,'LR-300 Assault Rifle','ranged','elite',400,80, 40,30,40,3,2.5,'semi-auto',3,30,2),
+(1,'AK47 Assault Rifle','ranged','elite',450,85, 45,35,45,3,3,'full-auto',3,30,3.5),
+(1,'Bolt Action Rifle','ranged','elite',900,150, 75,50,50,5,0.8,'single',3,4,5);
 
 INSERT INTO MeleeWeapons(maxStackSize, itemName, itemType, lootGrade,
 attackRange, headDamage,chestDamage,legDamage,armsDamage,
 treeGatherRate,oreGatherRate, fleshGatherRate,canBeThrown) VALUES
-('1','Machete','melee','tool','1.5','70','35','29','35','0','0','0.8',true),
-('1','Salvage Sword','melee','basic','1.5','100','50','45','50','0','0','0.8',true),
-('1','Salvage Cleaver','melee','basic','1.5','120','60','50','55','0','0','0.7',true),
-('1','Longsword','melee','military','2','150','75','36','45','0','0','0.6',true),
-('1','Mace','melee','basic','1.5','100','50','45','50','0','0','0',true),
-('1','Stone Spear','melee','basic','2.2','70','35','35','28','0','0','0',true),
-('1','Spear','melee','primary','2.2','30','25','25','13','0','0','0',true),
-('1','Rock','melee','resource','1.25','20','10','8','8','0.5','0.5','0.5',true),
-('1','Stone Hatchet','melee','resource','1.5','20','10','8','8','0.75','0','0.75',true),
-('1','Stone Pickaxe','melee','resource','1.5','20','10','8','8','0','0.75','0.75',true),
-('1','Hatchet','melee','resource','1.5','30','25','13','25','1','0','1',true),
-('1','Pickaxe','melee','resource','1.5','30','25','13','25','0','1','0.65',true);
+(1,'Machete','melee','tool',1.5,70,35,29,35,0,0,0.8,true),
+(1,'Salvage Sword','melee','basic',1.5,100,50,45,50,0,0,0.8,true),
+(1,'Salvage Cleaver','melee','basic',1.5,120,60,50,55,0,0,0.7,true),
+(1,'Longsword','melee','military',2,150,75,36,45,0,0,0.6,true),
+(1,'Mace','melee','basic',1.5,100,50,45,50,0,0,0,true),
+(1,'Stone Spear','melee','basic',2.2,70,35,35,28,0,0,0,true),
+(1,'Spear','melee','primary',2.2,30,25,25,13,0,0,0,true),
+(1,'Rock','melee','resource',1.25,20,10,8,8,0.5,0.5,0.5,true),
+(1,'Stone Hatchet','melee','resource',1.5,20,10,8,8,0.75,0,0.75,true),
+(1,'Stone Pickaxe','melee','resource',1.5,20,10,8,8,0,0.75,0.75,true),
+(1,'Hatchet','melee','resource',1.5,30,25,13,25,1,0,1,true),
+(1,'Pickaxe','melee','resource',1.5,30,25,13,25,0,1,0.65,true);
 
 INSERT INTO Clothing(maxStackSize, itemName, itemType, lootGrade,
 coldResistance,radResistance,explosionResistance,meleeResistance,
@@ -94,6 +94,57 @@ rangedResistance,biteResistance,equipmentSlot,wetResistance) VALUES
 (1,'Hazmat Suit','clothing','military',0.08,0.5,0.05,0.3,0.3,0.08,'whole-body-armour',0),
 (1,'Heavy Hazmat Suit','clothing','elite',0.12,0.7,0.1,0.5,0.5,0.15,'whole-body-armour',0);
 
+INSERT INTO Consumables(maxStackSize, itemName, itemType, lootGrade,
+instantHeal,healOverTime,hidrationYield,
+bleedingYield,radiationYield,poisonYield,hungerYield,vomitChance) VALUES
+(20,'Apple','consumable','food',2,0,15,0,0,0,0,30,0),
+(20,'Mushroom','consumable','food',3,0,5,0,0,0,0,15,0),
+(20,'Raspberry','consumable','food',10,0,20,0,0,0,-5,40,0),
+(20,'Blueberry','consumable','food',10,0,20,0,0,0,-5,30,0),
+(20,'Cactus Flesh','consumable','food',0,3,20,0,0,0,0,20,0),
+(20,'Tune Can','consumable','food',0,2,15,0,0,0,0,50,0),
+(20,'Can of Beans','consumable','food',0,4,25,0,0,0,0,100,0),
+(20,'Chocolate Bar','consumable','food',0,2,1,0,0,0,0,100,0),
+(20,'Chicken Breast','consumable','food',0,10,3,0,0,0,0,40,0),
+(20,'Deer Meat','consumable','food',0,10,3,0,0,0,0,40,0),
+(20,'Wolf Meat','consumable','food',0,5,1,0,0,0,0,60,0),
+(20,'Pork Meat','consumable','food',0,5,1,0,0,0,0,60,0),
+(20,'Bear Meat','consumable','food',0,5,1,0,0,0,0,100,0),
+(20,'Corn','consumable','food',0,6,5,0,0,0,0,40,0),
+(20,'Pumpkin','consumable','food',0,10,30,0,0,0,0,100,0),
+(5,'Water Bottle','consumable','food',0,0,200,-20,0,0,0,0,0),
+(10,'Anti-Radiation','consumable','food',0,0,-50,-25,0,0,0,0,0),
+(10,'Bandage','consumable','food',0,5,0,-5,0,0,-2,0,0),
+(5,'Medical Syringe','consumable','food',15,20,0,0,-10,0,0,-2,0),
+(2,'Large Medkit','consumable','food',0,100,-15,-15,-10,0,0,-10,0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 
 
 
