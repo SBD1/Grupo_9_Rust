fc403b6d524516e8b7324a69fbaa9e356591be02
gabrielbpn <180145088@aluno.unb.br>
2022-03-28 15:34:18 -0300

    populating biomes

---
+++ 4. SQL/scripts/map-region-biomes-pop.sql
@@ -0,0 +1,14 @@
+'INSERT INTO Map VALUES ();'
+
+'INSERT INTO Region VALUES ();'
+
+INSERT INTO Biomes VALUES ('1', 'xyz1', '1', '1','Temperate Grassland Plains', 'climate1');
+INSERT INTO Biomes VALUES ('2', 'xyz2', '1', '1','Temperate Grassland Hills', 'climate2');
+INSERT INTO Biomes VALUES ('3', 'xyz3', '1', '1','Temperate Forest', 'climate3');
+INSERT INTO Biomes VALUES ('4', 'xyz4', '1', '1','Desert', 'climate4');
+INSERT INTO Biomes VALUES ('5', 'xyz5', '1', '1','Snow', 'climate5');
+INSERT INTO Biomes VALUES ('6', 'xyz6', '1', '1','Trivia', 'climate6');
+INSERT INTO Biomes VALUES ('7', 'xyz7', '1', '1','Beach', 'climate7');
+INSERT INTO Biomes VALUES ('8', 'xyz8', '1', '1','Lake', 'climate8');
+INSERT INTO Biomes VALUES ('9', 'xyz9', '1', '1','River', 'climate9');
+INSERT INTO Biomes VALUES ('10', 'xyz10', '1', '1','Ocean', 'climate10');
