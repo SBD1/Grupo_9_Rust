0b1a481453238de7e93d6d5ccbdf010967aa2857
iagoomr <iagoomr@gmail.com>
2022-03-28 22:11:55 -0300

    atualizando sql

--- 4. SQL/CriacaoBD_Final.sql
+++ 4. SQL/CriacaoBD_Final.sql
@@ -1,4 +1,4 @@
-/* -------------------------------------------------- 
+/* --------------------------------------------------
 
 SGBD 1 - GRUPO 9
 
@@ -17,12 +17,12 @@ CREATE TYPE monument_tier AS ENUM('0','1','2','3');
 CREATE TYPE enemy_grade AS ENUM('scientist', 'outpost-scientist', 'heavy-scientist', 'bandit');
 CREATE TYPE ENUM_BOOLEAN AS ENUM('true','false');
 
-CREATE TABLE IF NOT EXISTS Maps ( 
+CREATE TABLE IF NOT EXISTS Maps (
     id SERIAL PRIMARY KEY UNIQUE,
     mapID int UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS Structures ( 
+CREATE TABLE IF NOT EXISTS Structures (
   structureID INTEGER NOT NULL,
   monument VARCHAR(100) NOT NULL,
   CONSTRAINT pk_Structures PRIMARY KEY(structureID)
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS Monuments (
     name VARCHAR(10) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS Region ( 
+CREATE TABLE IF NOT EXISTS Region (
     name VARCHAR(30),
     coordinates int,
     biome VARCHAR(30),
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS Region (
     FOREIGN KEY (monument) REFERENCES Monuments(name) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS HaveMapRegion ( 
+CREATE TABLE IF NOT EXISTS HaveMapRegion (
   mapID INTEGER NOT NULL UNIQUE,
   coordinates INTEGER NOT NULL,
   CONSTRAINT pk_HaveMapRegion PRIMARY KEY(mapID,coordinates),
@@ -54,15 +54,15 @@ CREATE TABLE IF NOT EXISTS HaveMapRegion (
   CONSTRAINT fk_coordinates_HaveMapRegion FOREIGN KEY (coordinates) REFERENCES Structures(structureID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS Climate ( 
+CREATE TABLE IF NOT EXISTS Climate (
   climateID int UNIQUE,
   temperature DECIMAL(4,1),
-  event VARCHAR(30), 
+  event VARCHAR(30),
   statusEffect VARCHAR(30),
   visibility ENUM_STATUS
 );
 
-CREATE TABLE IF NOT EXISTS Biomes ( 
+CREATE TABLE IF NOT EXISTS Biomes (
   biomesID int UNIQUE,
   coordinates int,
   resourceAbundance boolean,
@@ -71,40 +71,38 @@ CREATE TABLE IF NOT EXISTS Biomes (
   climate int
 );
 
-CREATE TABLE IF NOT EXISTS Flora ( 
+CREATE TABLE IF NOT EXISTS Flora (
   flora VARCHAR(30) NOT NULL,
   biomes INTEGER NOT NULL,
   CONSTRAINT pk_Flora PRIMARY KEY(flora),
   CONSTRAINT fk_Flora_Biomes FOREIGN KEY (biomes) REFERENCES Biomes(biomesID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS Fauna ( 
+CREATE TABLE IF NOT EXISTS Fauna (
   fauna VARCHAR(30) NOT NULL,
   biomes INTEGER NOT NULL,
   CONSTRAINT pk_Fauna PRIMARY KEY(fauna),
   CONSTRAINT fk_Fauna_Biomes FOREIGN KEY (biomes) REFERENCES Biomes(biomesID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS Characters ( 
+CREATE TABLE IF NOT EXISTS Characters (
   charactersID INTEGER NOT NULL UNIQUE,
   name VARCHAR(100) NOT NULL,
   position INTEGER NOT NULL,
   characterModel VARCHAR(30) NOT NULL,
-  climate INTEGER NOT NULL,
   type VARCHAR(30) NOT NULL,
   item VARCHAR(30) NOT NULL,
   CONSTRAINT pk_Characters PRIMARY KEY(charactersID),
-  CONSTRAINT fk_Characters_Climate FOREIGN KEY (climate) REFERENCES Climate(climateID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS GatherYield ( 
-  character INTEGER NOT NULL, 
+CREATE TABLE IF NOT EXISTS GatherYield (
+  character INTEGER NOT NULL,
   gatherYield VARCHAR(30) NOT NULL,
   CONSTRAINT pk_GatherYield PRIMARY KEY(character,gatherYield),
   CONSTRAINT fk_GatherYield_Characters FOREIGN KEY (character) REFERENCES Characters(charactersID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS EnterCombatCharacters ( 
+CREATE TABLE IF NOT EXISTS EnterCombatCharacters (
   enterCombatCharactersID INTEGER NOT NULL,
   firstCharacter INTEGER NOT NULL,
   secondCharacter INTEGER NOT NULL,
@@ -113,7 +111,7 @@ CREATE TABLE IF NOT EXISTS EnterCombatCharacters (
   CONSTRAINT fk_EnterCombatCharacters_Characters_2 FOREIGN KEY (secondCharacter) REFERENCES Characters(charactersID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS CombatLog ( 
+CREATE TABLE IF NOT EXISTS CombatLog (
   enterCombatCharacters INTEGER NOT NULL,
   indexLog INTEGER NOT NULL,
   CONSTRAINT pk_CombatLog PRIMARY KEY(enterCombatCharacters,indexLog),
@@ -123,12 +121,59 @@ CREATE TABLE IF NOT EXISTS CombatLog (
 CREATE TABLE IF NOT EXISTS Backpack (
     ownerID INT UNIQUE,
     id SERIAL PRIMARY KEY UNIQUE,
-    avaliableSlots INT,
-    slot SMALLINT,
-    totalSlots SMALLINT
+    slot01 INT,
+    slot02 INT,
+    slot03 INT,
+    slot04 INT,
+    slot05 INT,
+    slot06 INT,
+    slot07 INT,
+    slot08 INT,
+    slot09 INT,
+    slot10 INT,
+    slot11 INT,
+    slot12 INT,
+    slot13 INT,
+    slot14 INT,
+    slot15 INT,
+    slot16 INT,
+    slot17 INT,
+    slot18 INT,
+    slot19 INT,
+    slot20 INT,
+    slot21 INT,
+    slot22 INT,
+    slot23 INT,
+    slot24 INT,
+    slot25 INT,
+    slot26 INT,
+    slot27 INT,
+    slot28 INT,
+    slot29 INT,
+    slot30 INT,
+    slot31 INT,
+    slot32 INT,
+    slot33 INT,
+    slot34 INT,
+    slot35 INT,
+    slot36 INT,
+    slot37 INT,
+    slot38 INT,
+    slot39 INT,
+    slot40 INT,
+    slot41 INT,
+    slot42 INT,
+    slot43 INT,
+    slot44 INT,
+    slot45 INT,
+    slot46 INT,
+    slot47 INT,
+    slot48 INT,
+    slot49 INT,
+    slot50 INT,
 );
 
-CREATE TABLE IF NOT EXISTS PlayerCharacters ( 
+CREATE TABLE IF NOT EXISTS PlayerCharacters (
   charactersID INTEGER NOT NULL,
   hydration INTEGER NOT NULL,
   poisoned INTEGER NOT NULL,
@@ -145,7 +190,7 @@ CREATE TABLE IF NOT EXISTS PlayerCharacters (
   CONSTRAINT fk_PlayerCharacters_Backpack FOREIGN KEY (backpack) REFERENCES Backpack(ownerID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS RecruitableCharacters ( 
+CREATE TABLE IF NOT EXISTS RecruitableCharacters (
   charactersID INTEGER NOT NULL,
   specialization VARCHAR(30) NOT NULL,
   recruited ENUM_BOOLEAN NOT NULL,
@@ -153,7 +198,7 @@ CREATE TABLE IF NOT EXISTS RecruitableCharacters (
   CONSTRAINT fk_RecruitableCharacters_Characters FOREIGN KEY (charactersID) REFERENCES Characters(charactersID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS MainCharacter ( 
+CREATE TABLE IF NOT EXISTS MainCharacter (
   charactersID INTEGER NOT NULL,
   owner INTEGER NOT NULL,
   CONSTRAINT pk_MainCharacter PRIMARY KEY(charactersID,owner),
@@ -169,17 +214,17 @@ CREATE TABLE IF NOT EXISTS RespawnLocation (
     coordinateY VARCHAR(5)
 );
 
-CREATE TABLE IF NOT EXISTS NPCs ( 
+CREATE TABLE IF NOT EXISTS NPCs (
   charactersID INTEGER NOT NULL,
   isAgressive ENUM_BOOLEAN NOT NULL,
-  aggroRange VARCHAR(30) NOT NULL, 
+  aggroRange VARCHAR(30) NOT NULL,
   enemyGrade VARCHAR(30) NOT NULL,
   type VARCHAR(30) NOT NULL,
   CONSTRAINT pk_NPCs PRIMARY KEY(charactersID),
   CONSTRAINT fk_NPCs_Characters FOREIGN KEY (charactersID) REFERENCES Characters(charactersID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS Animals ( 
+CREATE TABLE IF NOT EXISTS Animals (
   charactersID INTEGER NOT NULL,
   sound VARCHAR(100) NOT NULL,
   modelType VARCHAR(30) NOT NULL,
@@ -187,14 +232,14 @@ CREATE TABLE IF NOT EXISTS Animals (
   CONSTRAINT fk_Animals_Characters FOREIGN KEY (charactersID) REFERENCES Characters(charactersID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS Scientists ( 
+CREATE TABLE IF NOT EXISTS Scientists (
   charactersID INTEGER NOT NULL,
   hasDialogue ENUM_BOOLEAN NOT NULL,
   CONSTRAINT pk_Scientists PRIMARY KEY(charactersID),
   CONSTRAINT fk_Scientists_Characters FOREIGN KEY (charactersID) REFERENCES Characters(charactersID) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS DialogueText ( 
+CREATE TABLE IF NOT EXISTS DialogueText (
   dialogue VARCHAR(100) NOT NULL,
   character INTEGER NOT NULL,
   CONSTRAINT pk_DialogueText PRIMARY KEY(dialogue),
@@ -205,11 +250,11 @@ CREATE TABLE IF NOT EXISTS Items (
 	id SERIAL primary key unique NOT NULL,
 	maxStackSize int NOT NULL,
 	itemName varchar(100) NOT NULL,
-	itemType item_type NOT NULL,	
+	itemType item_type NOT NULL,
 	lootGrade loot_grade
 );
 
-CREATE TABLE IF NOT EXISTS MeleeWeapons(
+CREATE TABLE IF NOT EXISTS MeleeWeapons (
 	attackRange int,
 	headDamage int,
 	chestDamage int,
@@ -221,7 +266,7 @@ CREATE TABLE IF NOT EXISTS MeleeWeapons(
 	canBeThrown boolean
 )INHERITS (items);
 
-CREATE TABLE IF NOT EXISTS RangedWeapons(
+CREATE TABLE IF NOT EXISTS RangedWeapons (
 	attackRange int,
 	headDamage int,
 	chestDamage int,
@@ -235,7 +280,7 @@ CREATE TABLE IF NOT EXISTS RangedWeapons(
 	recoil int
 )INHERITS (items);
 
-CREATE TABLE IF NOT EXISTS Consumables(
+CREATE TABLE IF NOT EXISTS Consumables (
 	instantHeal int,
 	healOverTime int,
 	hidrationYield int,
@@ -246,12 +291,12 @@ CREATE TABLE IF NOT EXISTS Consumables(
 	vomitChance numeric(4,2)
 )INHERITS(items);
 
-CREATE TABLE IF NOT EXISTS Teas(
+CREATE TABLE IF NOT EXISTS Teas (
 	statusUpgradeType varchar(15),
 	upgradePercentage numeric(4,2)
 )INHERITS(Consumables);
 
-CREATE TABLE IF NOT EXISTS Clothing(
+CREATE TABLE IF NOT EXISTS Clothing (
 	coldResistance numeric(4,2),
 	radResistance numeric(4,2),
 	explosionResistance numeric(4,2),
@@ -262,7 +307,7 @@ CREATE TABLE IF NOT EXISTS Clothing(
 	wetResistance numeric(4,2)
 )INHERITS(items);
 
-CREATE TABLE IF NOT EXISTS Resources(
+CREATE TABLE IF NOT EXISTS Resources (
 	isPrimary boolean NOT NULL,
 	isRefinable boolean NOT NULL
 )INHERITS (items);
@@ -274,7 +319,7 @@ CREATE TABLE IF NOT EXISTS ResourceNodes (
     durabilityDamage FLOAT
 );
 
-CREATE TABLE IF NOT EXISTS ResourceNodesGenerateItems ( 
+CREATE TABLE IF NOT EXISTS ResourceNodesGenerateItems (
   resourceNodes INTEGER NOT NULL,
   item INTEGER NOT NULL,
   CONSTRAINT pk_ResourceNodesGenerateItems PRIMARY KEY(resourceNodes, item),
@@ -282,7 +327,7 @@ CREATE TABLE IF NOT EXISTS ResourceNodesGenerateItems (
   CONSTRAINT fk_ResourceNodesGenerateItems_Items FOREIGN KEY (item) REFERENCES Items(id) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS RegionsMonuments ( 
+CREATE TABLE IF NOT EXISTS RegionsMonuments (
   name VARCHAR(30) NOT NULL,
   regionMonument VARCHAR(30) NOT NULL,
   CONSTRAINT pk_RegionsMonuments PRIMARY KEY(name, regionMonument),
@@ -295,7 +340,7 @@ CREATE TABLE IF NOT EXISTS LootCrates (
     grade loot_grade
 );
 
-CREATE TABLE IF NOT EXISTS StructuresContainsLootCrates ( 
+CREATE TABLE IF NOT EXISTS StructuresContainsLootCrates (
   structure INTEGER NOT NULL,
   lootCrates INTEGER NOT NULL,
   CONSTRAINT pk_StructuresContainsLootCrates PRIMARY KEY(structure, lootCrates),
@@ -303,7 +348,7 @@ CREATE TABLE IF NOT EXISTS StructuresContainsLootCrates (
   CONSTRAINT fk_StructuresContainsLootCrates_LootCrates FOREIGN KEY (lootCrates) REFERENCES LootCrates(id) ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS Party ( 
+CREATE TABLE IF NOT EXISTS Party (
   partyID INTEGER NOT NULL,
   character INTEGER NOT NULL,
   capacity INTEGER NOT NULL,
--- 4. SQL/PopulacaoBD_Final.sql
+++ 4. SQL/PopulacaoBD_Final.sql
@@ -1,4 +1,4 @@
-/* ------------------------------------------------- 
+/* -------------------------------------------------
 
 SGBD 1 - GRUPO 9
 
@@ -6,36 +6,79 @@ Script DML - População do banco de dados
 
 ---------------------------------------------------- */
 
-INSERT INTO Maps VALUES (1,1),(2,2),(3,3);
+INSERT INTO Maps VALUES
+(1,1),
+(2,2),
+(3,3);
 
-INSERT INTO Structures VALUES (1,'Structure1'),(2,'Structure2'),(3,'Structure3');
+INSERT INTO Structures VALUES
+(1,'Structure1'),
+(2,'Structure2'),
+(3,'Structure3');
 
-INSERT INTO Monuments VALUES (1,1,'0','scientist','lest','Monument1'),(1,2,'1','scientist','lest','Monument2'),(1,3,'2','scientist','lest','Monument3');
+INSERT INTO Monuments VALUES
+(1,1,'0','scientist','lest','Monument1'),
+(1,2,'1','scientist','lest','Monument2'),
+(1,3,'2','scientist','lest','Monument3');
 
-INSERT INTO Climate VALUES (1, 30.3, 'event1', 'status1'),(2, 5.4, 'event2', 'status2'),(3, 20.2, 'event3', 'status3');
+INSERT INTO Climate VALUES
+(1, 30.3, 'event1', 'status1'),
+(2, 5.4, 'event2', 'status2'),
+(3, 20.2, 'event3', 'status3');
 
-INSERT INTO Biomes VALUES (1, 1, false, true,'Temperate Grassland Plains', 1),(2, 1, false, true,'Temperate Grassland Hills', 2),(3, 1, false, true,'Temperate Forest', 3);
+INSERT INTO Biomes VALUES
+(1, 1, false, true,'Temperate Grassland Plains', 1),
+(2, 1, false, true,'Temperate Grassland Hills', 2),
+(3, 1, false, true,'Temperate Forest', 3);
 
-INSERT INTO Characters VALUES (1, 'Character1', '10', 'model1', 1, 'type1', 'item1'),(2, 'Character2', '5', 'model2', 2, 'type2', 'item2'),(3, 'Character3', '20', 'model3', 3, 'type3', 'item3');
+INSERT INTO Characters VALUES
+(1, 'Character1', '10', 'model1', 1, 'type1', 'item1'),
+(2, 'Character2', '5', 'model2', 2, 'type2', 'item2'),
+(3, 'Character3', '20', 'model3', 3, 'type3', 'item3');
 
-INSERT INTO Backpack VALUES (1, 1, 4, 2, 4),(2, 2, 4, 2, 4),(3, 3, 4, 2, 4);
+INSERT INTO Backpack VALUES
+(1, 1, 4, 2, 4),
+(2, 2, 4, 2, 4),
+(3, 3, 4, 2, 4);
 
-INSERT INTO PlayerCharacters VALUES (1, 10, 20, 30, 'type1', 1, 2, 3, 4, 5, 1),(2, 30, 20, 10, 'type2', 1, 5, 4, 3, 2, 2),(3, 30, 10, 20, 'type3', 1, 3, 5, 4, 2, 3);
+INSERT INTO PlayerCharacters VALUES
+(1, 10, 20, 30, 'type1', 1, 2, 3, 4, 5, 1),
+(2, 30, 20, 10, 'type2', 1, 5, 4, 3, 2, 2),
+(3, 30, 10, 20, 'type3', 1, 3, 5, 4, 2, 3);
 
-INSERT INTO RecruitableCharacters VALUES (1, 'Melee Weapons','true'),(2, 'Bows','false'),(3, 'Handguns','true');
+INSERT INTO RecruitableCharacters VALUES
+(1, 'Melee Weapons','true'),(2, 'Bows','false'),
+(3, 'Handguns','true');
 
-INSERT INTO MainCharacter VALUES (1, 1),(3, 2),(2, 3);
+INSERT INTO MainCharacter VALUES
+(1, 1),
+(3, 2),
+(2, 3);
 
-INSERT INTO NPCs VALUES (1, 'false', '30', '50', 'type1'),(2, 'true', '40', '60', 'type2'),(3, 'false', '80', '70', 'type3');
+INSERT INTO NPCs VALUES
+(1, 'false', '30', '50', 'type1'),
+(2, 'true', '40', '60', 'type2'),
+(3, 'false', '80', '70', 'type3');
 
-INSERT INTO Animals VALUES (1, 'roar, roooor', 'aligator'),(2, 'ssssss', 'snake'),(3, 'auuu', 'wolf');
+INSERT INTO Animals VALUES
+(1, 'roar, roooor', 'aligator'),
+(2, 'ssssss', 'snake'),
+(3, 'auuu', 'wolf');
 
-INSERT INTO Scientists VALUES (1, 'true'),(2, 'false'),(3, 'true');
-	
-INSERT INTO ResourceNodes (nodeType, maxYield, durabilityDamage) VALUES ('sulfur', 300, 7.5),('stone', 1000, 7.5),('metal',600, 7.5),('tree', 500, 3.75),('cactus', 3, 2.5);
+INSERT INTO Scientists VALUES
+(1, 'true'),
+(2, 'false'),
+(3, 'true');
+
+INSERT INTO ResourceNodes (nodeType, maxYield, durabilityDamage) VALUES
+('sulfur', 300, 7.5),
+('stone', 1000, 7.5),
+('metal',600, 7.5),
+('tree', 500, 3.75),
+('cactus', 3, 2.5);
 
 
-INSERT INTO Biomes VALUES 
+INSERT INTO Biomes VALUES
 ('1', 'xyz1', '1', '1','Temperate Grassland Plains', 'climate1'),
 ('2', 'xyz2', '1', '1','Temperate Grassland Hills', 'climate2'),
 ('3', 'xyz3', '1', '1','Temperate Forest', 'climate3'),
@@ -47,7 +90,7 @@ INSERT INTO Biomes VALUES
 ('9', 'xyz9', '1', '1','River', 'climate9'),
 ('10', 'xyz10', '1', '1','Ocean', 'climate10');
 
-INSERT INTO Resources (maxStackSize,isPrimary, itemType,itemName,isRefinable,lootGrade) VALUES 
+INSERT INTO Resources (maxStackSize,isPrimary, itemType,itemName,isRefinable,lootGrade) VALUES
 	(1000,true,'resource','Wood',False,'primary'),
 	(1000,true,'resource','Stone',False,'primary'),
 	(1000,true,'resource','Bone Fragments',False,'primary'),
@@ -66,7 +109,7 @@ INSERT INTO Resources (maxStackSize,isPrimary, itemType,itemName,isRefinable,loo
 	(1000,false,'resource','Charcoal',false,'resource'),
 	(1000,false,'resource','Gunpowder',false,'resource');
 
-INSERT INTO RangedWeapons(maxStackSize, itemName, itemType, lootGrade, 
+INSERT INTO RangedWeapons(maxStackSize, itemName, itemType, lootGrade,
 attackRange, headDamage,chestDamage,legDamage,armsDamage, accuracyModifier, fireRate,
 fireMode, modSlots, ammoCapacity, recoil) VALUES
 (1,'Hunting Bow','ranged','basic',150,80, 50,35,40,0.5,0.5,'single',0,1,0),
@@ -169,7 +212,7 @@ instantHeal,healOverTime,hidrationYield,bleedingYield,radiationYield,poisonYield
 INSERT INTO TEAS (maxStackSize, itemName, itemType, lootGrade,
 instantHeal,healOverTime,hidrationYield,
 bleedingYield,radiationYield,poisonYield,hungerYield,vomitChance,
-statusUpgradeType, upgradePercentage) VALUES 
+statusUpgradeType, upgradePercentage) VALUES
 (5,'Normal Max Health Tea','tea','food',0,0,0,0,0,0,0,0,'health',0.05),
 (5,'Advanced Max Health Tea','tea','food',0,0,0,0,0,0,0,0,'health',0.125),
 (5,'Pure Max Health Tea','tea','food',0,0,0,0,0,0,0,0,'health',0.2),
