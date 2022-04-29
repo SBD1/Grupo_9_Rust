79d40d0665d9972b7ed56ae69280bbca9b47957d
Thiago <thiagoiferreira@outlook.com>
2022-04-29 16:34:26 -0300

    FIxing backpack query code, fixing bugs in the code that were generating consistent errors

--- app/game_instances.py
+++ app/game_instances.py
@@ -8,7 +8,7 @@ import random
 class game_instances():
     def load_game_db(self):
         DBUSER= os.getenv('DBUSER','postgres')
-        DBPASSWORD = os.getenv('DBPASSWORD','postgres')
+        DBPASSWORD = os.getenv('DBPASSWORD','shadow954')
         DBHOST = os.getenv('DBHOST','localhost')
         DBPORT = os.getenv('DBPORT','5432')
         DBNAME = os.getenv('DBNAME','rust-2')
@@ -22,8 +22,8 @@ class game_instances():
         monument_content = []
         for square in range(monument_size):
             if(square == 4 or square == 9 or square == 14 or square == 19):
-                structure = pd.read_sql("select * from structures ORDER BY random() LIMIT 1;".format(loot_grade),engine)
-                monument_content.append(['structure',structure.id,structure.combat_enemy,structure.name])
+                structure = pd.read_sql("select * from structures ORDER BY random() LIMIT 1;",engine)
+                monument_content.append(['structure',structure.id[0],structure.combat_enemy[0],structure.name[0]])
             elif(square ==  0):
                 monument_content.append(['loot_box',game_instances.instance_loot_box(self,loot_grade,engine)])
             else:
@@ -45,17 +45,20 @@ class game_instances():
 
     def instance_loot_box(self,loot_grade,engine):
         query = "select * from lootcrates where grade = '{}'".format(loot_grade)
-        qoi = pd.read_sql(query,engine).item_quantity
-        
-        iqoi = random.randint(2,int(qoi))
-        
+        qoi = pd.read_sql(query,engine).item_quantity.get(key=0)
+
+        try:
+            iqoi = random.randint(1,qoi)
+        except:
+            iqoi = 1
+
         query = """SELECT id FROM items WHERE lootgrade = '{}' ORDER BY random() LIMIT {};  """.format(loot_grade,iqoi)
         
         lootbox_items = pd.read_sql(query, engine)
-        
-        columns = pd.read_sql('select * from loot_crate_instance',engine).columns
-        
-        if iqoi == 2:
+
+        if iqoi == 1:
+            insertion = "INSERT INTO loot_crate_instance(loot_grade,item1_id) VALUES ('{}',{})".format(loot_grade, lootbox_items.id[0])
+        elif iqoi == 2:
             insertion = "INSERT INTO loot_crate_instance(loot_grade,item1_id,item2_id) VALUES ('{}',{},{})".format(loot_grade, lootbox_items.id[0],lootbox_items.id[1])
         elif iqoi == 3:
             insertion = "INSERT INTO loot_crate_instance(loot_grade,item1_id,item2_id,item3_id) VALUES ('{}',{},{},{})".format(loot_grade, lootbox_items.id[0],lootbox_items.id[1],lootbox_items.id[2])
@@ -67,12 +70,12 @@ class game_instances():
             insertion = "INSERT INTO loot_crate_instance(loot_grade,item1_id,item2_id,item3_id,item4_id,item5_id,item6_id) VALUES ('{}',{},{},{},{},{},{})".format(loot_grade, lootbox_items.id[0],lootbox_items.id[1],lootbox_items.id[2],lootbox_items.id[3],lootbox_items.id[4],lootbox_items.id[5])
         elif iqoi == 7:
             insertion = "INSERT INTO loot_crate_instance(loot_grade,item1_id,item2_id,item3_id,item4_id,item5_id,item6_id,item7_id) VALUES ('{}',{},{},{},{},{},{},{})".format(loot_grade, lootbox_items.id[0],lootbox_items.id[1],lootbox_items.id[2],lootbox_items.id[3],lootbox_items.id[4],lootbox_items.id[5],lootbox_items.id[6])        
-                             
+                            
         with engine.begin() as conn:     # TRANSACTION
             conn.execute(insertion)
             
         return int(pd.read_sql("select id from loot_crate_instance order by id desc limit 1;",engine).id)
-    
+
     def quit_game(self,engine):
         drop_query = 'DELETE FROM loot_crate_instance;'
         with engine.begin() as conn:     # TRANSACTION
@@ -80,9 +83,8 @@ class game_instances():
         engine.dispose()
 
     def getBackpack(self, characterId, engine):
-        character = []
-        character.append(pd.read_sql(f"select * from Backpack where ownerID={characterId} LIMIT 1;",engine))
-        return character
+        backpack = pd.read_sql("select * from Backpack where ownerID={} LIMIT 1;".format(characterId),engine)
+        return backpack
 
     def equipBackpackItem(self, characterId, itemId, engine):
         item = []
